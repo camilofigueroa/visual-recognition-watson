0c178feda775d438a8a351f57f9c2e7aa463837d
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2020-05-29 13:44:36 -0500

    Versión con Bootstrap.
    Esta es una versión que tiene lo básico, para trabajar con el modelo vista controlador y el Bootstrap.

---
+++ controlador/inicio.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * Autor: Camilo Figueroa ( Crivera )
+     * Controlador: sección inicio.
+     *
+     */
+
+    $seccion = "../vista/inicio.phtml";
+    
+    include( "../vista/plantilla.phtml" );
+    
+    
